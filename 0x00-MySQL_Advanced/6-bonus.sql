8caf89dfbeec74dad5e751c0ccb6de11916f0ac3
Trmrskr <simonalale@gmail.com>
2023-07-13 17:15:18 +0000

    Add bonus: minor edits

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -10,9 +10,9 @@ BEGIN
     IF (SELECT COUNT(name) FROM projects WHERE name = project_name) = 0
     THEN
         INSERT INTO projects (name) VALUES (project_name);
-    END IF
+    END IF;
     SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
 END
 $$
 DELIMITER ;
