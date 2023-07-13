0fcbb6ce08c00c80037bbb5e5a8c477fbbd0611f
Trmrskr <simonalale@gmail.com>
2023-07-13 14:54:22 +0000

    We are all unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,8 @@
+-- Create a table user having columns id, email, name, and primary key equals id
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     PRIMARY KEY (id)
 );
+
