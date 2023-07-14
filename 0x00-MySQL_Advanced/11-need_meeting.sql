054bbbd38f748d1749623f10683f889ad83f6de6
Trmrskr <simonalale@gmail.com>
2023-07-14 16:31:37 +0000

    changed = to is

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,5 +4,5 @@ CREATE VIEW need_meeting AS
 SELECT name FROM students
 WHERE
     score < 80 AND
-    (last_meeting = NULL OR
+    (last_meeting IS NULL OR
     (DATEDIFF(MONTH, last_meeting, CURDATE()) >= 1));
