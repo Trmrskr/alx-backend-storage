69eb135929e48f50a96b98cf5877350b807b1e03
Trmrskr <simonalale@gmail.com>
2023-07-18 08:05:45 -0700

    log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Python script provides some stats about Nginx logs
+stored in MongoDB
+"""
+from pymongo import MongoClient
+
+
+if __name__ == '__main__':
+    """The main namespace"""
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    collections = client.logs.nginx
+    print("{} logs".format(collections.count_documents({})))
+    print("Methods:")
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        counted = collections.count_documents({"method": method})
+        print("    method {}: {}".format(method, counted))
+
+    get_status_count = collections.count_documents(
+        {"method": methods[0], "path": "/status"})
+    print("{} status check".format(get_status_count))
