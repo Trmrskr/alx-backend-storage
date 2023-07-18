fa7cdc0ebad032e06a9d6edecd64b809ef94c535
Trmrskr <simonalale@gmail.com>
2023-07-18 09:15:11 -0700

    Top students

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""MongoDB Operations with python using pymongo"""
+""" MongoDB Operations with Python using pymongo """
 
 
 def top_students(mongo_collection):
+    # sourcery skip: inline-immediately-returned-variable
     """ Returns all students sorted by average score """
-    top_student = mongo_collection.aggregrate([
+    top_student = mongo_collection.aggregate([
         {
             "$project": {
                 "name": "$name",
